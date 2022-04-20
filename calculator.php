e0508784c8777a21e4f69310bea4ef154d088ec7
srinjay kapri <66597192+Srinja333@users.noreply.github.com>
2022-04-20 12:54:56 +0530

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <meta charset ="utf-8">
+                <title>  </title>
+        </head>
+      <body>
+      <form  action="calculator.php" method="get">
+          Number1:
+           <input type="number" name="num1">
+          <br>
+          Number2:
+            <input type="number" name="num2">
+          <input type= "submit">
+     </form>
+     <br>
+        sum : 
+        <?php echo $_GET["num1"] + $_GET["num2"] ?>
+      </body>
+</html>
