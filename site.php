57e46cdb4283ce5e4db7aa521efc705c50c4e38c
srinjay kapri <66597192+Srinja333@users.noreply.github.com>
2022-04-20 11:25:26 +0530

    Update site.php

--- site.php
+++ site.php
@@ -6,28 +6,28 @@
         </head>
       <body>
         <?php 
-            /*we can rite php stanalone like in between <?php...?> but here we use php in between html for use html advantages
+            /*we can write php stanalone like in between <?php...?> but here we use php in between html for use html advantages
               
           always write code in php inbetween<?php.....?>*/
-
-          /*$name="srinjay's";//'$'use for variable declearation here string use so " " used
+              
+         //introduction to variable and data types 
+          $name="srinjay's";//'$'use for variable declearation here string use so " " used
           $age=21;//integer type so no " "
           echo("<h1>$name own site</h1><br>");// echo used for print
           echo"<hr>";
           echo"<p>$name age = $age</p><br>";
-          $name="digantaleena";
+          $name="doggy";
           $age=23;
           echo("<h3>srinjay loved $name</h3><br>");
-          echo"<p>her age = $age</p><br>";*/
+          echo"<p>her age = $age</p><br>";
 
-
-          /*$str=null/ null means no value but u can use "any string";
+           $str=null/ null means no value but u can use "any string";
            $num=null/null means no value but  u can use any value;
            $iamhere=true;
           echo "$str<br>";
-          echo $num;*/
+          echo $num;
 
-
+        //playing with strings
           $string="i am boss";
           echo strtoupper($string);//use for make string in upper case
           echo"<br>";
@@ -45,6 +45,44 @@
           echo str_replace("boss","Goat",$string);//its replace boss in string by goat
           echo"<br>";
           echo substr($string,5,3);//1st argument is the string, 2nd arg for where to start, 3rd arg  use for how much grabing number of characters
+              
+          //playing with numbers    
+          echo 40;
+          echo "<br>";
+          echo 40.998;
+          echo "<br>";
+          echo 5-9;
+          echo "<br>";
+          echo (4+5)*10;
+          echo "<br>";
+          $num=90;
+          echo $num++;//it gives 90 , but ++$num gives 91 if we use it in  echo section
+          echo "<br>";
+          echo $num;// it gives 91
+          echo "<br>";
+          $num++;
+          echo $num;//it gives 92
+          echo "<br>";
+          $num=$num+90;//its as same as $num += 90, we can use instead of + also -,*,%,/
+          echo $num;
+          echo "<br>";
+          echo abs(-100);//use to give absolute values
+          echo "<br>";
+          echo pow(2,4); // it gives 2 to the poer 4
+          echo "<br>";
+          echo sqrt(144);// gives square root
+          echo "<br>";
+          echo max(2,10*9);//give max no
+          echo "<br>";
+          echo min(80,90*2);//give min no
+          echo "<br>";
+          echo round(3.2);//it gives 3
+          echo "<br>";
+          echo round(3.7);//it gives 4
+          echo "<br>";
+          echo ceil(3.7);//rounded up give 4
+          echo "<br>";
+          echo floor(3.9);//rounded down give 3
         ?>
      </body>
 </html>
