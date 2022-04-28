0da6b94c3797c654f001faeee7e50a781b89de8c
srinjay kapri <66597192+Srinja333@users.noreply.github.com>
2022-04-28 13:06:36 +0530

    Add files via upload

---
+++ array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <meta charset ="utf-8">
+                <title>  </title>
+        </head>
+      <body>
+
+      
+        <?php 
+        $friends= array("ayan","raju","digantaleena","aryan");
+        $friends[4]=9;
+        echo $friends[4];
+         ?>
+        
+       
+           </body>
+      </html>
