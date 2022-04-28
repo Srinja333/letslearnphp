a9127a3d65f3f11ce74865a62c02a06047ed5421
srinjay kapri <66597192+Srinja333@users.noreply.github.com>
2022-04-28 13:41:44 +0530

    Add files via upload

---
+++ function.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <meta charset ="utf-8">
+                <title>  </title>
+        </head>
+      <body>
+      
+     
+        <?php 
+        function sayHi($name,$age)
+        {
+            echo "hello $name and you are $age <br>";
+        }
+        sayHi("ayan",55);
+        sayHi("srinja",54);
+        ?>
+</body>
+</html>
