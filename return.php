1edb9874ec42fadc2ed022ece7aa368d0f75ba4c
srinjay kapri <66597192+Srinja333@users.noreply.github.com>
2022-05-03 13:17:36 +0530

    Add files via upload

---
+++ return.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <meta charset ="utf-8">
+                <title>  </title>
+        </head>
+      <body>
+      
+     
+        <?php 
+        function cube($num)
+        {
+            echo "hello <br>";
+            return $num*$num*$num;
+        }
+         echo cube(4);
+        ?>
+</body>
+</html>
